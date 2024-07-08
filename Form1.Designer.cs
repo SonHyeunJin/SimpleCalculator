8d6bcd33f6d3b3027e866b8d0d2c5168a259604f
sonhyeunjin <dksjejrl123@naver.com>
2024-07-08 22:50:45 +0900

    equal

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -93,7 +93,7 @@
             this.btnEqual.TabIndex = 36;
             this.btnEqual.Text = "=";
             this.btnEqual.UseVisualStyleBackColor = true;
-            
+            this.btnEqual.Click += new System.EventHandler(this.btnEqual_Click);
             // 
             // btnPlus
             // 
